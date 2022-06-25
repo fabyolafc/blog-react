37374293a79dd2bb7713a248c3a2e4ea22225730
Fabyola <fabyycmp@gmail.com>
2022-06-24 21:01:17 -0300

    Blog Pessoal

---
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import'./Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className='titulo'>Home</h1>
+            <img src="https://user-images.githubusercontent.com/92693153/143438192-e48f8c22-82d0-4a0e-9279-7c9a890aafed.png" alt='avatar' className='img'/>
+        </>
+    )
+}
+
+export default Home;
