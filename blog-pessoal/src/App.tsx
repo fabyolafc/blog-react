40c5454a3049502caf103ac51b58e455ca319068
Fabyola <fabyycmp@gmail.com>
2022-07-04 09:07:56 -0300

    Comunicação com o Back-end - Login e Cadastro

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -2,26 +2,26 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
-import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
+import Home from './paginas/home/Home';
 import './App.css';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 
 function App() {
-  return(
+  return (
     <Router>
-    <Navbar />
-    <div style={{ minHeight: '100vh' }}>
-    <Routes> // Antigo Switch
-    <Route path="/" element={<Login />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/home" element={<Home />} />
-    
-    </Routes>
-    </div>
-    <Footer />
+        <Navbar />
+        <div style={{ minHeight: '100vh' }}>
+          <Routes> // Antigo Switch
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} /> 
+            <Route path="/home" element={<Home />} />
+            <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+          </Routes>
+        </div>
+      <Footer />
     </Router>
-    )
+  );
 }
-//<Route path="/cadastro" element={<CadastroUsuario />} />
 
-export default App;
+export default App; 
--- blog-pessoal/src/components/estaticos/footer/Footer.tsx
+++ blog-pessoal/src/components/estaticos/footer/Footer.tsx
@@ -13,7 +13,7 @@ function Footer() {
                 <Grid alignItems="center" item xs={12}>
                     <Box className='box1'>
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom className='textos'>Siga-nos nas redes sociais </Typography>
+                            <Typography variant="h5" align="center" gutterBottom className='textos'>Siga-me nas redes sociais </Typography>
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
                             <a href="https://m.facebook.com/fabyola.campos.54" target="_blank">
