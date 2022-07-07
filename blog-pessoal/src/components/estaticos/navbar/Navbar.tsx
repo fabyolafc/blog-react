d8198e9691ae7185aba12be01bad0517d1415456
Fabyola <fabyycmp@gmail.com>
2022-07-07 09:47:52 -0300

    Blog Pessoal

--- blog-pessoal/src/components/estaticos/navbar/Navbar.tsx
+++ blog-pessoal/src/components/estaticos/navbar/Navbar.tsx
@@ -1,56 +1,73 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography} from '@material-ui/core';
-import {Box} from '@mui/material';
-import MenuBookIcon from '@mui/icons-material/MenuBook';
-import { Link } from 'react-router-dom';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { Box } from '@mui/material';
+import LocalFloristIcon from '@mui/icons-material/LocalFlorist';
+import { Link, useNavigate } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
 import './Navbar.css';
 
 function Navbar() {
-  return (
-    <>
-        <AppBar position="static">
+    const [token, setToken] = useLocalStorage('token');
+    let navigate = useNavigate();
+
+    function goLogout() {
+        setToken('')
+        alert("Usuário deslogado")
+        navigate('/login')
+    }
+
+    return (
+        <>
+            <AppBar position="static">
                 <Toolbar variant="dense" className='cor'>
-                <MenuBookIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                    <LocalFloristIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
                     <Box className='cursor'>
                         <Typography variant="h5" color="inherit">
-                            Faby Blog
+                            FABY BLOG
                         </Typography>
                     </Box>
 
-                    <Box display="flex" justifyContent="start" paddingLeft="15px">
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                Home
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                Postagens
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                Temas
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                Cadastrar Tema
-                            </Typography>
-                        </Box>
-                        <Link to='/login' className='text-decorator-none'>
+                    <Box className='menuBar' display="flex" justifyContent="start">
+                        <Link to="/home" className="text-decorator-none">
                             <Box mx={1} className='cursor'>
                                 <Typography variant="h6" color="inherit">
-                                    Logout
+                                    Home
                                 </Typography>
                             </Box>
                         </Link>
+                        <Link to="/posts" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    Postagens
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Link to="/temas" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    Temas
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Link to="/formularioTema" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    Cadastrar Tema
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Box mx={1} className='cursor' onClick={goLogout}>
+                            <Typography variant="h6" color="inherit">
+                                Logout
+                            </Typography>
+                        </Box>
+
                     </Box>
 
                 </Toolbar>
             </AppBar>
-    </>
-  )
+        </>
+    )
 }
 
 export default Navbar
--- blog-pessoal/src/components/temas/listatema/ListaTema.tsx
+++ blog-pessoal/src/components/temas/listatema/ListaTema.tsx
@@ -1,46 +1,78 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import { Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
 import { Box } from '@mui/material';
+import Tema from '../../../models/Tema';
 import './ListaTema.css';
+import useLocalStorage from 'react-use-localstorage';
+import {useNavigate} from 'react-router-dom';
+import { busca } from '../../../services/Service';
 
 function ListaTema() {
+  const [temas, setTemas] = useState<Tema[]>([])
+  const [token, setToken] = useLocalStorage('token');
+  let navigate = useNavigate();
 
-    return (
-        <>
-            <Box m={2} >
-                <Card variant="outlined">
-                    <CardContent>
-                        <Typography color="textSecondary" gutterBottom>
-                            Tema
-                        </Typography>
-                        <Typography variant="h5" component="h2">
-                            Minha descrição
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Box display="flex" justifyContent="center" mb={1.5} >
+  useEffect(()=>{
+    if(token == ''){
+      alert("Você precisa estar logado")
+      navigate("/login")
+    }
+  }, [token])
 
-                            <Link to="" className="text-decorator-none">
-                                <Box mx={1}>
-                                    <Button variant="contained" className="marginLeft button" size='small' color="primary" >
-                                        atualizar
-                                    </Button>
-                                </Box>
-                            </Link>
-                            <Link to="" className="text-decorator-none">
-                                <Box mx={1}>
-                                    <Button className='button' variant="contained" size='small' color="secondary">
-                                        deletar
-                                    </Button>
-                                </Box>
-                            </Link>
-                        </Box>
-                    </CardActions>
-                </Card>
+
+  async function getTema(){
+    await busca("/temas", setTemas, {
+      headers: {
+        'Authorization': token
+      }
+    })
+  }
+
+
+  useEffect(()=>{
+    getTema()
+  }, [temas.length])
+
+  return (
+    <>
+    {
+      temas.map(tema =>(
+      <Box m={2} >
+        <Card variant="outlined">
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Tema
+            </Typography>
+            <Typography variant="h5" component="h2">
+             {tema.descricao}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Box display="flex" justifyContent="center" mb={1.5} >
+
+              <Link to={`/formularioTema/${tema.id}`} className="text-decorator-none">
+                <Box mx={1}>
+                  <Button variant="contained" className="marginLeft button" size='small' color="primary" >
+                    atualizar
+                  </Button>
+                </Box>
+              </Link>
+              <Link to={`/deletarTema/${tema.id}`} className="text-decorator-none">
+                <Box mx={1}>
+                  <Button className='button' variant="contained" size='small' color="secondary">
+                    deletar
+                  </Button>
+                </Box>
+              </Link>
             </Box>
-        </>
-    );
+          </CardActions>
+        </Card>
+      </Box>
+      ))
+      }
+    </>
+  );
 }
 
 
